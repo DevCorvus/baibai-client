de05790ce9e7a3133aa5c0a9f65768ff030546a0
DevCorvus <devcorvus.contact@gmail.com>
2023-02-17 13:43:47 -0400

    feat: Basic navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,21 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar bg-primary text-slate-50 flex justify-between">
+      <div>
+        <Link to={'/'} className="btn btn-ghost text-xl">
+          Products
+        </Link>
+      </div>
+      <ul className="menu menu-horizontal">
+        <li>
+          <Link to={'/login'}>Login</Link>
+        </li>
+        <li>
+          <Link to={'/register'}>Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
